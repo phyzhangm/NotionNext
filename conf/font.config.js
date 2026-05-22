94e4f5bce579830c11b93f6655685484967918e6
phyzhangm <119054186+phyzhangm@users.noreply.github.com>
2026-05-22 10:23:24 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -1,16 +1,20 @@
 // START ************网站字体*****************
+// ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+// 后面空格隔开的 font-light 是字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
+FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
 
-FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
 // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
+// 如果需要引入第三方字体，可以在此添加URL
 FONT_URL: [
-  'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+  // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
   'https://fonts.googleapis.com/css?family=Bitter&display=swap',
   'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
   'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
 ],
-// 无衬线字体 例如'"LXGW WenKai"'
+
+// 无衬线字体 例如'"LXGW WenKai"' , 如果字体有包含空格，需要用 "双 引 号" 包起来。
 FONT_SANS: [
-  '"LXGW WenKai"',
+  // '"LXGW WenKai"',
   '"PingFang SC"',
   '-apple-system',
   'BlinkMacSystemFont',
@@ -28,9 +32,11 @@ FONT_SANS: [
   'sans-serif',
   '"Apple Color Emoji"'
 ],
+
+
 // 衬线字体 例如'"LXGW WenKai"'
 FONT_SERIF: [
-  '"LXGW WenKai"',
+  // '"LXGW WenKai"',
   'Bitter',
   '"Noto Serif SC"',
   'SimSun',
